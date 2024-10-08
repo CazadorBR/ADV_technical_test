a5c9218a194655aa680304884c1cc1c9d9bcc784
Ben Romdhane Fedi <fedi.benromdhane@gmail.com>
2024-10-08 16:04:03 +0100

    6*

--- src/app/api/auth/authOptions.ts
+++ src/app/api/auth/authOptions.ts
@@ -14,54 +14,54 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
-  callbacks: {
-    async signIn({ user }) {
-      try {
-        const existingUser = await prisma.user.findUnique({
-          where: { email: user.email || "" },
-        });
+  // callbacks: {
+  //   async signIn({ user }) {
+  //     try {
+  //       const existingUser = await prisma.user.findUnique({
+  //         where: { email: user.email || "" },
+  //       });
 
-        if (!existingUser) {
-          await prisma.user.create({
-            data: {
-              email: user.email || "",
-              nom: user.name || "",
-              prenom: user.name?.split(" ")[0] || "",
-              naissance: "23/07/2001", // Remplacer par une valeur réelle
-              numero: "55428615", // Remplacer par une valeur réelle
-            },
-          });
-        }
+  //       if (!existingUser) {
+  //         await prisma.user.create({
+  //           data: {
+  //             email: user.email || "",
+  //             nom: user.name || "",
+  //             prenom: user.name?.split(" ")[0] || "",
+  //             naissance: "23/07/2001", // Remplacer par une valeur réelle
+  //             numero: "55428615", // Remplacer par une valeur réelle
+  //           },
+  //         });
+  //       }
 
-        return true;
-      } catch (error) {
-        console.error("Erreur lors de l'enregistrement de l'utilisateur:", error);
-        return false;
-      }
-    },
-    async session({ session }) {
-      const dbUser = await prisma.user.findUnique({
-        where: { email: session.user?.email || "" },
-        select: {
-          id: true,
-          nom: true,
-          prenom: true,
-          naissance: true,
-          numero: true,
-          adresse: true,
-        },
-      });
+  //       return true;
+  //     } catch (error) {
+  //       console.error("Erreur lors de l'enregistrement de l'utilisateur:", error);
+  //       return false;
+  //     }
+  //   },
+  //   async session({ session }) {
+  //     const dbUser = await prisma.user.findUnique({
+  //       where: { email: session.user?.email || "" },
+  //       select: {
+  //         id: true,
+  //         nom: true,
+  //         prenom: true,
+  //         naissance: true,
+  //         numero: true,
+  //         adresse: true,
+  //       },
+  //     });
 
-      if (dbUser) {
-        session.user.id = String(dbUser.id);
-        session.user.nom = dbUser.nom || "";
-        session.user.prenom = dbUser.prenom || "";
-        session.user.naissance = dbUser.naissance || "";
-        session.user.numero = dbUser.numero ? String(dbUser.numero) : "";
-        session.user.adresse = dbUser.adresse || "";
-      }
+  //     if (dbUser) {
+  //       session.user.id = String(dbUser.id);
+  //       session.user.nom = dbUser.nom || "";
+  //       session.user.prenom = dbUser.prenom || "";
+  //       session.user.naissance = dbUser.naissance || "";
+  //       session.user.numero = dbUser.numero ? String(dbUser.numero) : "";
+  //       session.user.adresse = dbUser.adresse || "";
+  //     }
 
-      return session;
-    },
-  },
+  //     return session;
+  //   },
+  // },
 };
--- src/app/api/auth/getUser.ts
+++ src/app/api/auth/getUser.ts
@@ -1,4 +1,4 @@
-import prisma from "@/app/lib/prisma";
+ import prisma from '@/app/lib/prisma';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/react';
  
